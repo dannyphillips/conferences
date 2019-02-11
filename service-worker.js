1b07e9ba67c2934b39eb60de1162c9422cc8df3f
Danny Phillips <danny.phillips8@gmail.com>
2019-02-11 16:46:46 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/conferences/precache-manifest.f3f5a80462a001e2e1fe955a76d5707b.js"
+  "/conferences/precache-manifest.3fab307e0b517010b9404f305cce13bc.js"
 );
 
 workbox.clientsClaim();
