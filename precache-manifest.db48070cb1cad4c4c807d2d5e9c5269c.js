1f14f9581bed10a94db52ffad894f127932511ad
Danny Phillips <danny.phillips8@gmail.com>
2019-05-10 04:48:26 +0000

    Updates

---
+++ precache-manifest.db48070cb1cad4c4c807d2d5e9c5269c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2c8dd5c4f37f934a535",
+    "url": "/conferences/static/js/runtime~main.c2c8dd5c.js"
+  },
+  {
+    "revision": "116bcb3c08810669f1ba",
+    "url": "/conferences/static/js/main.116bcb3c.chunk.js"
+  },
+  {
+    "revision": "57310d3ebac08069d512",
+    "url": "/conferences/static/js/1.57310d3e.chunk.js"
+  },
+  {
+    "revision": "116bcb3c08810669f1ba",
+    "url": "/conferences/static/css/main.691a26a9.chunk.css"
+  },
+  {
+    "revision": "57310d3ebac08069d512",
+    "url": "/conferences/static/css/1.ce57c8bb.chunk.css"
+  },
+  {
+    "revision": "bffc15b09a2fb91a8865fe9a234c7456",
+    "url": "/conferences/index.html"
+  }
+];
